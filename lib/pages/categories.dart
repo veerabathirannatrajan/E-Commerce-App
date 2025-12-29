7ab0811f8c5da91da4551ab6fe98c80e452b623c
VeeraBathiran <veerabathirannatrajan@gmail.com>
2025-12-29 13:26:05 +0530

    4th commit

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -9,130 +9,160 @@ class categories extends StatefulWidget {
 }
 
 class _categoriesState extends State<categories> {
-  List categories = [
-    {'name': 'New', 'image': 'new.png'},
-    {'name': 'Clothes', 'image': 'clothes.png'},
+  List<Map<String, String>> menCategories = [
+    {'name': 'Clothes', 'image': 'Shirt (2).png'},
+    {'name': 'Shoes', 'image': 'men_shoes.jpg'},
+    {'name': 'Accessories', 'image': 'men_acc.jpg'},
+    {'name': 'Watches', 'image': 'men_watch.jpg'},
+  ];
+
+  List<Map<String, String>> womenCategories = [
+    {'name': 'Tops', 'image': 'Blouse.png'},
+    {'name': 'Dresses', 'image': 'Longsleeve Violeta.png'},
     {'name': 'Shoes', 'image': 'shoes.png'},
     {'name': 'Accessories', 'image': 'accesories.png'},
   ];
 
-  int selectedTab = 0;
+  List<Map<String, String>> kidsCategories = [
+    {'name': 'T-Shirts', 'image': 'kid_ts.jpg'},
+    {'name': 'Shorts', 'image': 'kid_shorts.jpg'},
+    {'name': 'Shoes', 'image': 'kid_shoe.jpg'},
+    {'name': 'Toys', 'image': 'toys.jpg'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     final w = MediaQuery.of(context).size.width;
     final h = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        title: Text(
-          'Categories',
-          style: TextStyle(
-            fontSize: w * 0.07,
-            letterSpacing: 2,
-            fontWeight: FontWeight.w800,
-            color: Colors.black,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.arrow_back_ios),
+            splashColor: Colors.transparent,
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          title: Text(
+            'Categories',
+            style: TextStyle(
+              fontSize: w * 0.08,
+              letterSpacing: 2,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search, size: w * 0.07, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Men'),
+              Tab(text: 'Women'),
+              Tab(text: 'Kids'),
+            ],
+            indicatorColor: Colors.red,
+            labelStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+            ),
+            labelColor: Colors.black,
           ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search, size: w * 0.07, color: Colors.black),
-            onPressed: () {},
-          ),
-        ],
-      ),
-
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: w * 0.03),
-        children: [
-
-          // ================== CATEGORY TABS ==================
-          Row(
-            children: List.generate(3, (index) {
-              final titles = ['Women', 'Men', 'Kids'];
-              return Expanded(
-                child: InkWell(
-                  onTap: () {
-                    setState(() => selectedTab = index);
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: h * 0.06,
-                        child: Center(
-                          child: Text(
-                            titles[index],
-                            style: TextStyle(
-                              fontSize: w * 0.055,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 4,
-                        color: selectedTab == index
-                            ? Colors.red
-                            : Colors.transparent,
-                      )
-                    ],
-                  ),
-                ),
-              );
-            }),
-          ),
-
-          SizedBox(height: h * 0.03),
-
-          // ================== SALE BANNER ==================
-          Container(
-            height: h * 0.14,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: TabBarView(
+          children: [
+            // ================= Men Tab =================
+            ListView(
+              padding: EdgeInsets.symmetric(horizontal: w * 0.03, vertical: h * 0.02),
               children: [
-                Text(
-                  'SUMMER SALES',
-                  style: TextStyle(
-                    fontSize: w * 0.07,
-                    letterSpacing: 2,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'Up to 50% off',
-                  style: TextStyle(
-                    fontSize: w * 0.045,
-                    letterSpacing: 1.5,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
+                saleBanner(w, h,60),
+
+                SizedBox(height: h * 0.02),
+                ...menCategories.map(
+                      (product) => catCard(
+                        name: product['name']!,
+                        image: product['image']!,
+                      ),
                 ),
               ],
             ),
-          ),
 
-          SizedBox(height: h * 0.02),
+            // ================= Women Tab =================
+            ListView(
+              padding: EdgeInsets.symmetric(horizontal: w * 0.03, vertical: h * 0.02),
+              children: [
+                saleBanner(w, h, 30),
 
-          // ================== CATEGORY LIST ==================
-          ...categories.map((product) {
-            return catCard(
-              name: product['name'],
-              image: product['image'],
-            );
-          }).toList(),
-        ],
+                SizedBox(height: h * 0.02),
+                ...womenCategories.map(
+                      (product) => catCard(
+                        name: product['name']!,
+                        image: product['image']!,
+                      ),
+                ),
+              ],
+            ),
+
+            // ================= Kids Tab =================
+            ListView(
+              padding: EdgeInsets.symmetric(horizontal: w * 0.03, vertical: h * 0.02),
+              children: [
+                saleBanner(w, h , 20),
+
+                SizedBox(height: h * 0.02),
+                ...kidsCategories.map(
+                      (product) => catCard(
+                        name: product['name']!,
+                        image: product['image']!,
+                      ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+Widget saleBanner(double w, double h,int a) {
+  return Container(
+    height: h * 0.14,
+    decoration: BoxDecoration(
+      color: Colors.red,
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'SUMMER SALES',
+          style: TextStyle(
+            fontSize: w * 0.07,
+            letterSpacing: 2,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          'Up to $a% off',
+          style: TextStyle(
+            fontSize: w * 0.045,
+            letterSpacing: 1.5,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
--- lib/services/categories_card.dart
+++ lib/services/categories_card.dart
@@ -12,7 +12,9 @@ class catCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get screen dimensions
     final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
 
     return Material(
       color: Colors.transparent,
@@ -21,10 +23,13 @@ class catCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         onTap: () {},
         child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-          height: 160,
-          width: width, // 👈 take full available width
-          padding: const EdgeInsets.all(8),
+          margin: EdgeInsets.symmetric(
+              vertical: height * 0.012,
+              horizontal: width * 0.02
+          ),
+          height: height * 0.2, // responsive height
+          width: width, // full width
+          padding: EdgeInsets.all(width * 0.02), // responsive padding
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(12),
@@ -33,11 +38,11 @@ class catCard extends StatelessWidget {
             children: [
               // LEFT TEXT SECTION
               Expanded(
-                flex: 1,
+                flex: 6,
                 child: Container(
-                  padding: const EdgeInsets.only(left: 20),
+                  padding: EdgeInsets.only(left: width * 0.05),
                   decoration: BoxDecoration(
-                    color: Colors.grey[300],
+                    color: Colors.grey[100],
                     borderRadius: const BorderRadius.horizontal(
                       left: Radius.circular(12),
                     ),
@@ -47,7 +52,7 @@ class catCard extends StatelessWidget {
                     name,
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                      fontSize: width * 0.06, // responsive text
+                      fontSize: width * 0.06, // responsive font size
                       letterSpacing: 2,
                       fontWeight: FontWeight.w800,
                       color: Colors.black,
@@ -58,12 +63,13 @@ class catCard extends StatelessWidget {
 
               // RIGHT IMAGE SECTION
               Expanded(
-                flex: 1,
+                flex: 6,
                 child: Container(
+                  height: height * 0.18, // responsive image height
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage('assets/images/$image'),
-                      fit: BoxFit.fitWidth,
+                      fit: BoxFit.cover,
                     ),
                     borderRadius: const BorderRadius.horizontal(
                       right: Radius.circular(12),
